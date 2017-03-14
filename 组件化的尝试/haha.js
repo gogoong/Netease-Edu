98b2a9652a9baba047030819a29bb659013601e4
notebook myl <272201252@qq.com>
2017-03-14 16:12:07 +0800

    Ajax未异步,待解决,bindFunction待研究

--- 组件化的尝试/haha.js
+++ 组件化的尝试/haha.js
@@ -1,3 +1,10 @@
+var addEvent = document.addEventListener ?
+        function(ele,type,listener,useCapture){
+          ele.addEventListener(type,listener,useCapture);
+        }:
+        function(ele,type,listener){
+          ele.attachEvent('on'+type,listener);
+        };
   // 将HTML转换为节点
 function html2node(str){
   var container = document.createElement('div');
@@ -37,52 +44,168 @@ function get(url,options,callback){
   xhr.onreadystatechange = function(){
     if(xhr.readyState == 4){
       if((xhr.status >= 200 && xhr.status<300) || (xhr.status == 304 )){
-        callback(JSON.parse(xhr.responseText));
+        // callback(JSON.parse(xhr.responseText));
+        haha = JSON.parse(xhr.responseText);
       }
     }
   }
-  xhr.open('get',url + '?' + serialize(options),true);
+  xhr.open('get',url + '?' + serialize(options),false);
   xhr.send(null);
 }
 
 // courselist
 // ----
-
+   
 var template = 
-"<a class='img'>\
-  <img width = '223px' height = '124px'>\
-  <div class='detail'>\
-   <div class='top f-cb'>\
-      <img class='dimg' width = '223px' height = '124px'>\
-      <div class='content'>\
-        <div class='dttl'></div>\
-        <div class='dlcount icn'></div>\
-        <div class='dprv'></div>\
-        <div class='dcategory'></div>\
+  "<li class='u-course'>\
+    <a class='img'>\
+      <img width = '223px' height = '124px'>\
+      <div class='detail'>\
+       <div class='top f-cb'>\
+          <img class='dimg' width = '223px' height = '124px'>\
+          <div class='content'>\
+            <div class='dttl'></div>\
+            <div class='dlcount icn'></div>\
+            <div class='dprv'></div>\
+            <div class='dcategory'></div>\
+          </div>\
+        </div>\
+        <div class='descr'></div>\
       </div>\
-    </div>\
-    <div class='descr'></div>\
-  </div>\
-</a>\
-<div class='ttl'></div>\
-<div class='prv'></div>\
-<div class='lcount icn'></div>\
-<div class='price'></div> ";
+    </a>\
+  <div class='ttl'></div>\
+  <div class='prv'></div>\
+  <div class='lcount icn'></div>\
+  <div class='price'></div>\
+  </li>"
 
-function Courselist(options){
+// var courselist = document.querySelector(".m-courselist");
+function Course(options){
+
   options = options || {};
-  this.container = this._layout.cloneNode(true);
+
+  this.container = document.querySelector(".m-courselist");
+
   this.coursecount = this.container.getElementsByClassName("u-course");
-  this.getc = function(){
-    get(this.url,this.data,function(obj){
-      this.obj = obj
-    })
-  }
+
+  this.data = {pageNo:1,psize:20,type:10};
+
+  this.page = document.querySelector(".m-page"),
+
+  this.pagecount = document.getElementsByClassName("pageindex");
+
   extend(this,options)
+
+  this._initEvent();
+
 }
 
-extend(Courselist.prototype,{
+Course.prototype = { addEvent:function(ele,type,listener,useCapture){
+  if(ele.addEventListener){
+    ele.addEventListener(type,listener,useCapture);
+  }else{
+    ele.attachEvent('on'+type,listener);
+  }
+}}
+
+extend(Course.prototype,{
+
+  bindFunction : function(obj, func){
+    return function(){
+      func.apply(obj, arguments);
+    }
+  },
+
   _layout: html2node(template),
 
+  addcourse:function(i){
+
+    this.container.appendChild(this._layout.cloneNode(true));
+    this.changecourse(i);
+
+  },
+
+  changecourse:function(i){
+
+     this.container.children[i].children[0].firstElementChild.src = haha.list[i].middlePhotoUrl;
+     this.container.children[i].children[0].lastElementChild.firstElementChild.children[0].src = haha.list[i].middlePhotoUrl;
+     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[0].textContent = haha.list[i].name;
+     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[1].textContent = haha.list[i].learnerCount + "人在学"
+     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[2].textContent = "发布者:" + haha.list[i].provider;
+     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[3].textContent = "分类:" + haha.list[i].categoryName;
+     this.container.children[i].children[0].lastElementChild.lastElementChild.textContent = haha.list[i].description;
+     this.container.children[i].children[1].textContent = haha.list[i].name
+     this.container.children[i].children[2].innerHTML = haha.list[i].provider;
+     this.container.children[i].children[3].innerHTML = haha.list[i].learnerCount;
+     this.container.children[i].children[4].innerHTML = haha.list[i].price == 0? "免费" : '￥'+ haha.list[i].price;
+  },
+
+  pager:function(e){
+    var index = e.target.dataset.index,
+        pageNo = this.data.pageNo;
+    if(index<0 && (pageNo > 1? this.data.pageNo -=1 : false)){
+
+      this._initEvent();
+
+    }
+    
+    else if(index == 0 && (pageNo <haha.totalPage?this.data.pageNo +=1 : false)){
+      this._initEvent();
+    }
+    else if(index>0 && index != pageNo){
+      this.data.pageNo = index;
+      this._initEvent();
+    }else{
+      //
+    }
+    // console.log(this.data)
+    
+  },
+
+  _initEvent:function(){
+    get(this.url,this.data);
+    if(this.coursecount.length == 0){
+      for(var i = 0,length=haha.list.length;i<length;i++){
+        this.addcourse(i);
+      }
+    }else if(this.coursecount.length == haha.list.length){
+      for(var i = 0,length=haha.list.length;i<length;i++){
+        this.changecourse(i)
+      }
+    }else if(this.coursecount.length>haha.list.length){
+      for(var i = 0,length=haha.list.length;i<length;i++){
+        this.changecourse(i)
+      }
+      for(var i=haha.list.length,length=this.coursecount.length;i<length;i++){
+        this.container.removeChild(this.container.lastElementChild)
+      }
+    }else{
+      for(var i = 0;i<this.coursecount.length;i++){
+        this.changecourse(i)
+      }
+      for(var i=this.coursecount.length;i<haha.list.length;i++){
+        this.addcourse(i);
+      }
+    }
+    if(this.pagecount.length == 0){
+      for(var i=0 ,length = haha.totalPage;i<length;i++){
+        var pageindex = document.createElement("div");
+        // pageindex.className = "pageindex";
+        (i+1) == this.data.pageNo ? pageindex.className = "pageindex z-sel" : pageindex.className = "pageindex";
+        pageindex.setAttribute("data-index",i+1);
+        pageindex.innerHTML = i+1 ;
+        this.page.insertBefore(pageindex,this.page.lastElementChild);
+        // this.addEvent(pageindex,"click",this.bindFunction(this,this.pager(i)));
+        // this.addEvent(pageindex,"click",this.pager)
+        // this.addEvent(pageindex,"click",this.bindFunction(this,this.pager));
+      }
+      this.addEvent(this.page,"click",this.bindFunction(this,this.pager));
+    }
+    // this.addEvent(this.page,"click",this.bindFunction(this,this.pager));
+    for(i=0;i<haha.totalPage;i++){
+      (i+1) == this.data.pageNo ? this.pagecount[i].className = "pageindex z-sel" : this.pagecount[i].className = "pageindex";
+    }
+  },
 
 })
+
