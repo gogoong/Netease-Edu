9e6dfad9d4098f7e03fca6b6be02d6b21c46c885
notebook myl <272201252@qq.com>
2017-03-15 15:29:07 +0800

    完成热门课程模块

--- 组件化的尝试/haha.js
+++ 组件化的尝试/haha.js
@@ -1,3 +1,4 @@
+// 事件注册函数,兼容IE8
 var addEvent = document.addEventListener ?
         function(ele,type,listener,useCapture){
           ele.addEventListener(type,listener,useCapture);
@@ -53,10 +54,10 @@ function get(url,options,callback){
   xhr.send(null);
 }
 
-// courselist
+// course课程模块
 // ----
    
-var template = 
+var templateC = 
   "<li class='u-course'>\
     <a class='img'>\
       <img width = '223px' height = '124px'>\
@@ -77,49 +78,40 @@ var template =
   <div class='prv'></div>\
   <div class='lcount icn'></div>\
   <div class='price'></div>\
-  </li>"
+  </li>";
 
-// var courselist = document.querySelector(".m-courselist");
 function Course(options){
 
   options = options || {};
-
+  // 主容器
   this.container = document.querySelector(".m-courselist");
-
+  // 当前页课程数
   this.coursecount = this.container.getElementsByClassName("u-course");
-
-  // this.url = "https://study.163.com/webDev/couresByCategory.htm";
-
-  // this.data = {pageNo:1,psize:20,type:10};
-
+  // 页码器
   this.page = document.querySelector(".m-page"),
-
+  // 页数
   this.pagecount = document.getElementsByClassName("pageindex");
 
   extend(this,options);
 
   this._initEvent();
-  // this.say();
 
 }
 
 
 
 extend(Course.prototype,{
-  say:function(){
-    console.log(this)
-  },
 
-  _layout: html2node(template),
-
+  _layout: html2node(templateC),
+  // 增加课程
   addcourse:function(i){
 
     this.container.appendChild(this._layout.cloneNode(true));
-    this.changecourse(i);
+    this.setcourse(i);
 
   },
-
-  changecourse:function(i){
+  // 设置课程样式
+  setcourse:function(i){
 
      this.container.children[i].children[0].firstElementChild.src = this.list[i].middlePhotoUrl;
      this.container.children[i].children[0].lastElementChild.firstElementChild.children[0].src = this.list[i].middlePhotoUrl;
@@ -133,142 +125,161 @@ extend(Course.prototype,{
      this.container.children[i].children[3].innerHTML = this.list[i].learnerCount;
      this.container.children[i].children[4].innerHTML = this.list[i].price == 0? "免费" : '￥'+ this.list[i].price;
   },
-
+  // 页码器
   pager:function(event){
     if(event.target.tagName == "LI"){
-    var index = event.target.dataset.index,
-        pageNo = data.pageNo;
-    if(index == -1){
-      if(pageNo>1){
-      data.pageNo = data.pageNo - 1;
-      get(url,data,function(obj){
-        extend(obj,data)
-        list = new Course(obj)
-      })
+      var index = Number(event.target.dataset.index),
+          pageNo = data.pageNo;
 
-      // this._initEvent();
+      // -1为上一页,0为下一页
+      switch(index){
+        case -1:
+          if(pageNo>1){
+            data.pageNo = data.pageNo - 1;
+            get(url,data,function(obj){
+              extend(obj,data)
+              list = new Course(obj)
+            })
+          }
+        
+        break;
+        case 0:
+          if(pageNo<list.totalPage){
+            data.pageNo += 1;
+            get(url,data,function(obj){
+              extend(obj,data);
+              list = new Course(obj);
+            })
+          }
+        
+        break;
+        default:
+          if(index>0 && index != pageNo){
+            data.pageNo = index;
+            get(url,data,function(obj){
+              extend(obj,data);
+              list = new Course(obj);
+            })
+          }
       }
-    }
-    
-    else if(index == 0 ){
-      if(pageNo <list.totalPage){
-        data.pageNo = data.pageNo+1
-      get(url,data,function(obj){
-        extend(obj,data)
-        list = new Course(obj)
-      })
-      // this._initEvent();
-    }
-  }
-    else if(index>0 && index != pageNo){
-      data.pageNo = Number(index);
-      get(url,data,function(obj){
-        extend(obj,data)
-        list = new Course(obj)
-      })
-      // this._initEvent();
-    }else{
-      //
-    }
-    // console.log(this.data)
     }
   },
 
+  // 初始化事件,根据现有的课程数和获取到的课程数来增删/设置课程
   _initEvent:function(){
-    // get(this.url,this.data,function(obj){
-    //   list = new Course;
-    // })
+
     if(this.coursecount.length == 0){
       for(var i = 0,length=this.list.length;i<length;i++){
         this.addcourse(i);
       }
     }else if(this.coursecount.length == this.list.length){
       for(var i = 0,length=this.list.length;i<length;i++){
-        this.changecourse(i)
+        this.setcourse(i)
       }
     }else if(this.coursecount.length>this.list.length){
       for(var i = 0,length=this.list.length;i<length;i++){
-        this.changecourse(i)
+        this.setcourse(i)
       }
       for(var i=this.list.length,length=this.coursecount.length;i<length;i++){
         this.container.removeChild(this.container.lastElementChild)
       }
     }else{
       for(var i = 0;i<this.coursecount.length;i++){
-        this.changecourse(i)
+        this.setcourse(i)
       }
       for(var i=this.coursecount.length;i<this.list.length;i++){
         this.addcourse(i);
       }
     }
+    // 设置页码数
     if(this.pagecount.length == 0){
       for(var i=0 ,length = this.totalPage;i<length;i++){
         var pageindex = document.createElement("li");
-        // pageindex.className = "pageindex";
-        (i+1) == data.pageNo ? pageindex.className = "pageindex z-sel" : pageindex.className = "pageindex";
+        (i+1) == this.pageNo ? pageindex.className = "pageindex z-sel" : pageindex.className = "pageindex";
         pageindex.setAttribute("data-index",i+1);
         pageindex.innerHTML = i+1 ;
         this.page.insertBefore(pageindex,this.page.lastElementChild);
-        // this.addEvent(pageindex,"click",this.bindFunction(this,this.pager(i)));
-        // this.addEvent(pageindex,"click",this.pager)
-        // this.addEvent(pageindex,"click",this.bindFunction(this,this.pager));
-      // }
-      // this.addEvent(this.page,"click",this.bindFunction(this,this.pager));
-        // this.pager.bind(this);
+        /**  对页码器进行事件代理
+             这里有个疑问,原想法
+             this.pager.bind(this);addEvent(this.page,"click",this)
+             当点击翻页或页码时,会执行2次pager,还请老师解答一下 **/
         addEvent(this.page,"click",this.pager);
+        // 注册tab的点击事件
         var coursetab = document.getElementsByClassName("u-tab");
-  addEvent(coursetab[0].firstElementChild,"click",function(){
-  data.type = 10;
-  data.pageNo = 1;
-  coursetab[0].firstElementChild.className = "z-sel";
-  coursetab[0].lastElementChild.className = "";
-  get(url,data,function(obj){
-    extend(obj,data)
-    list = new Course(obj)
-  });
-});
-  addEvent(coursetab[0].lastElementChild,"click",function(){
-  data.type = 20;
-  data.pageNo = 1;
-  coursetab[0].lastElementChild.className = "z-sel";
-  coursetab[0].firstElementChild.className = "";
-  get(url,data,function(obj){
-    extend(obj,data)
-    list = new Course(obj);
-  });
-})
+        addEvent(coursetab[0].firstElementChild,"click",function(){
+          data.type = 10;
+          data.pageNo = 1;
+          coursetab[0].firstElementChild.className = "z-sel";
+          coursetab[0].lastElementChild.className = "";
+          get(url,data,function(obj){
+            extend(obj,data)
+            list = new Course(obj)
+          });
+        });
+        addEvent(coursetab[0].lastElementChild,"click",function(){
+          data.type = 20;
+          data.pageNo = 1;
+          coursetab[0].lastElementChild.className = "z-sel";
+          coursetab[0].firstElementChild.className = "";
+          get(url,data,function(obj){
+            extend(obj,data)
+            list = new Course(obj);
+          });
+        })
   
+      }
     }
-    }
-    // this.addEvent(this.page,"click",this.bindFunction(this,this.pager));
+    // 设置页码状态
     for(i=0;i<this.totalPage;i++){
       (i+1) == this.pageNo ? this.pagecount[i].className = "pageindex z-sel" : this.pagecount[i].className = "pageindex";
     }
-    
-}
-  // later:function(){
-  //   var data = {pageNo:this.pageNo,psize:this.psize,type:this.type};
-  //   get(this.url,data,function(obj){
-  //     list = new Course(obj);
-  //   })
-  //    if(this.coursecount.length == this.list.length){
-  //     for(var i = 0,length=this.list.length;i<length;i++){
-  //       this.changecourse(i)
-  //     }
-  //   }else if(this.coursecount.length>this.list.length){
-  //     for(var i = 0,length=this.list.length;i<length;i++){
-  //       this.changecourse(i)
-  //     }
-  //     for(var i=this.list.length,length=this.coursecount.length;i<length;i++){
-  //       this.container.removeChild(this.container.lastElementChild)
-  //     }
-  //   }else{
-  //     for(var i = 0;i<this.coursecount.length;i++){
-  //       this.changecourse(i)
-  //     }
-  //     for(var i=this.coursecount.length;i<this.list.length;i++){
-  //       this.addcourse(i);
-  //     }
-  //   }
-  // },
+  }
 })
+
+// 热门课程模块
+//
+var templateHotC = "<li class=u-hot f-cb>\
+                    <img width='50px' height='50px'>\
+                    <div>\
+                      <div class='cttl'></div>\
+                      <div class='lcount icn'></div>\
+                    </div>\
+                  </li>";
+
+function Hotcourse(options){
+  options = options || {};
+  // 将返回的数组放入list中,再放入Hotcourse
+  this.list = [];
+
+  extend(this.list,options);
+
+  this.container = document.querySelector(".hot").children[0];
+
+  this.supcontainer = this.container.parentNode;
+
+  this._initEvent();
+
+}
+
+extend(Hotcourse.prototype,{
+
+  _layout:html2node(templateHotC),
+  // 设置课程样式
+  setcourse:function(i){
+
+    this.container.children[i].firstElementChild.src = this.list[i].smallPhotoUrl;
+
+    this.container.children[i].lastElementChild.firstElementChild.textContent = this.list[i].name;
+
+    this.container.children[i].lastElementChild.lastElementChild.textContent = this.list[i].learnerCount;
+  },
+  // 初始化事件
+  _initEvent:function(){
+    for(var i = 0,length=this.list.length;i<length;i++){
+      list.addcourse.call(this,i)
+    }
+    // 克隆一个节点,用于后期滚动
+    this.supcontainer.appendChild(this.container.cloneNode(true));
+  }
+})
+
