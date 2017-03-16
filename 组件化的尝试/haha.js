1378108e5a0f448494e5d49ec3b32811e2be518f
notebook myl <272201252@qq.com>
2017-03-16 16:34:20 +0800

    完成轮播图模块,热门课程模块滚动动作有问题,to be fixed,学习了setTimeout.call的用法

--- 组件化的尝试/haha.js
+++ 组件化的尝试/haha.js
@@ -12,6 +12,25 @@ function html2node(str){
   container.innerHTML = str;
   return container.children[0];
 }
+// setTimeout,setInterval关于'this'的问题.
+// 直接setInterval.call(this,this.func,1000)会报错,setInterval运行环境中的this还是指向window
+// MDN给出了解决方案
+// 代码来源:https://developer.mozilla.org/zh-CN/docs/Web/API/Window/setInterval
+var __nativeST__ = window.setTimeout, __nativeSI__ = window.setInterval;
+
+window.setTimeout = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
+  var oThis = this, aArgs = Array.prototype.slice.call(arguments, 2);
+  return __nativeST__(vCallback instanceof Function ? function () {
+    vCallback.apply(oThis, aArgs);
+  } : vCallback, nDelay);
+};
+
+window.setInterval = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
+  var oThis = this, aArgs = Array.prototype.slice.call(arguments, 2);
+  return __nativeSI__(vCallback instanceof Function ? function () {
+    vCallback.apply(oThis, aArgs);
+  } : vCallback, nDelay);
+};
 
 // 赋值属性
 // extend({a:1}, {b:1, a:2}) -> {a:1, b:1}
@@ -46,7 +65,6 @@ function get(url,options,callback){
     if(xhr.readyState == 4){
       if((xhr.status >= 200 && xhr.status<300) || (xhr.status == 304 )){
         callback(JSON.parse(xhr.responseText));
-        // list = JSON.parse(xhr.responseText);
       }
     }
   }
@@ -112,18 +130,19 @@ extend(Course.prototype,{
   },
   // 设置课程样式
   setcourse:function(i){
-
-     this.container.children[i].children[0].firstElementChild.src = this.list[i].middlePhotoUrl;
-     this.container.children[i].children[0].lastElementChild.firstElementChild.children[0].src = this.list[i].middlePhotoUrl;
-     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[0].textContent = this.list[i].name;
-     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[1].textContent = this.list[i].learnerCount + "人在学"
-     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[2].textContent = "发布者:" + this.list[i].provider;
-     this.container.children[i].children[0].lastElementChild.firstElementChild.lastElementChild.children[3].textContent = "分类:" + (this.list[i].categoryName?this.list[i].categoryName:"无");
-     this.container.children[i].children[0].lastElementChild.lastElementChild.textContent = this.list[i].description;
-     this.container.children[i].children[1].textContent = this.list[i].name
-     this.container.children[i].children[2].innerHTML = this.list[i].provider;
-     this.container.children[i].children[3].innerHTML = this.list[i].learnerCount;
-     this.container.children[i].children[4].innerHTML = this.list[i].price == 0? "免费" : '￥'+ this.list[i].price;
+    var s = this.container.children[i],
+        l = this.list[i];
+    s.children[0].firstElementChild.src = l.middlePhotoUrl;
+    s.children[0].lastElementChild.firstElementChild.children[0].src = l.middlePhotoUrl;
+    s.children[0].lastElementChild.firstElementChild.lastElementChild.children[0].textContent = l.name;
+    s.children[0].lastElementChild.firstElementChild.lastElementChild.children[1].textContent = l.learnerCount + "人在学"
+    s.children[0].lastElementChild.firstElementChild.lastElementChild.children[2].textContent = "发布者:" + l.provider;
+    s.children[0].lastElementChild.firstElementChild.lastElementChild.children[3].textContent = "分类:" + (l.categoryName?l.categoryName:"无");
+    s.children[0].lastElementChild.lastElementChild.textContent = l.description;
+    s.children[1].textContent = l.name
+    s.children[2].innerHTML = l.provider;
+    s.children[3].innerHTML = l.learnerCount;
+    s.children[4].innerHTML = l.price == 0? "免费" : '￥'+ l.price;
   },
   // 页码器
   pager:function(event){
@@ -167,27 +186,28 @@ extend(Course.prototype,{
 
   // 初始化事件,根据现有的课程数和获取到的课程数来增删/设置课程
   _initEvent:function(){
-
-    if(this.coursecount.length == 0){
-      for(var i = 0,length=this.list.length;i<length;i++){
+    var clength = this.coursecount.length,
+        llength = this.list.length;
+    if(clength == 0){
+      for(var i = 0,length=llength;i<length;i++){
         this.addcourse(i);
       }
-    }else if(this.coursecount.length == this.list.length){
-      for(var i = 0,length=this.list.length;i<length;i++){
+    }else if(clength == llength){
+      for(var i = 0,length=llength;i<length;i++){
         this.setcourse(i)
       }
-    }else if(this.coursecount.length>this.list.length){
-      for(var i = 0,length=this.list.length;i<length;i++){
+    }else if(clength>llength){
+      for(var i = 0,length=llength;i<length;i++){
         this.setcourse(i)
       }
-      for(var i=this.list.length,length=this.coursecount.length;i<length;i++){
+      for(var i=llength,length=clength;i<length;i++){
         this.container.removeChild(this.container.lastElementChild)
       }
     }else{
-      for(var i = 0;i<this.coursecount.length;i++){
+      for(var i = 0;i<clength;i++){
         this.setcourse(i)
       }
-      for(var i=this.coursecount.length;i<this.list.length;i++){
+      for(var i=clength;i<llength;i++){
         this.addcourse(i);
       }
     }
@@ -257,6 +277,8 @@ function Hotcourse(options){
 
   this.supcontainer = this.container.parentNode;
 
+  this._mt = 0;
+
   this._initEvent();
 
 }
@@ -273,13 +295,116 @@ extend(Hotcourse.prototype,{
 
     this.container.children[i].lastElementChild.lastElementChild.textContent = this.list[i].learnerCount;
   },
+  // 滚动排行榜
+  scroll:function(){
+
+    this._mt += -70;
+
+    var str = "margin-top:"+this._mt +"px;" + "transition-property:margin-top;transition-duration:1s;transition-timing-function:linear";
+    
+    this.container.style.cssText = str;
+    //  这个写法不行,why? 和下面的轮播图几乎一样的思路代码,就是多了个if判断?
+    // if(this._mt == -140){
+    //   setTimeout.call(this,(function(){
+    //     this._mt =0;
+    //     this.container.style.cssText = "";
+    //   }),0)
+    // }
+  },
+  // 开始滚动
+  start:function(){
+    this.timer = setInterval.call(this,this.scroll,5000)
+  },
+  // 停止
+  stop:function(){
+    clearInterval(this.timer);
+  },
   // 初始化事件
   _initEvent:function(){
+    // 调用Course的addcourse函数
     for(var i = 0,length=this.list.length;i<length;i++){
       list.addcourse.call(this,i)
     }
     // 克隆一个节点,用于后期滚动
     this.supcontainer.appendChild(this.container.cloneNode(true));
+
+    this.start();
   }
+
 })
 
+// slider
+// 
+var templateS = "<div class='m-slider'>\
+                  <img class='slide'>\
+                  <div class='pointer'>\
+                    <i class='u-p' data-index='1'></i>\
+                    <i class='u-p' data-index='2'></i>\
+                    <i class='u-p' data-index='3'></i>\
+                  </div>\
+                </div>"
+function Slider(options){
+  extend(this,options);
+
+  options = options || {};
+
+  this.container = this.container || document.body;
+
+  this.slider = this._layout.cloneNode(true);
+
+  this.slides = this.slider.querySelector(".slide");
+
+  this.pointer = this.slider.querySelector(".pointer")
+
+  this.pointes = this.slider.querySelectorAll(".u-p");
+
+  this.pageNum = this.images.length;
+  // 判断pageindex是否合法/输入,默认为1
+  this.pageindex = (0<this.pageindex && this.pageindex<this.pageNum +1)? this.pageindex : 1;
+
+  this.container.appendChild(this.slider);
+
+  this._initEvent();
+}
+
+extend(Slider.prototype,{
+
+  _layout:html2node(templateS),
+
+  change:function(){
+    index = this.pageindex;
+    index ++;
+    index >3?index=1:index=index;
+    for(var i = 0,length=this.pageNum;i<length;i++){
+      this.pointes[i].className = "u-p";
+    }
+    // 当前"圆点"添加"z-crt"类名
+    this.pointes[index-1].className += " z-crt";
+    // 改变src,实现轮播
+    this.slides.src = this.images[index-1];
+    // 淡入效果
+    this.slides.style.cssText = "opacity:0;";
+    // 直接写this.slides...,而不用setTimeout会没有淡入效果
+    // 原因:出于性能，浏览器会尽可能的把最近的需要渲染的动作整合到一起执行
+    // 代码来源:segmentfault.com/q/1010000008704894 我自己的提问^ ^
+    setTimeout.call(this,(function(){
+      this.slides.style.cssText = "opacity:1;transition-property:opacity;transition-duration:0.5s;transition-timing-function:ease-in;"
+    }),0)
+
+    this.pageindex = index;
+  },
+  // 开始轮播
+  start:function(){
+    this.timer = setInterval.call(this,this.change,5000);
+  },
+  // 停止轮播
+  stop:function(){
+    clearInterval(this.timer);
+  },
+  // 初始化,设置'首页'图片以及对应"圆点"样式,开始轮播
+  _initEvent:function(){
+    this.slides.src = this.images[this.pageindex-1];
+    this.pointes[this.pageindex-1].className+=" z-crt";
+    this.start();
+  }
+})
